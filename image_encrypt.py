f8e72092cc5b866aa02401452116f04b5177f7f3
MourizRamadass <mourizramadass@gmail.com>
2026-06-20 21:38:23 +0530

    Create image_encrypt.py for image encryption
    Added a script for image encryption using pixel manipulation.

---
+++ image_encrypt.py
@@ -0,0 +1,14 @@
+# Image Encryption using Pixel Manipulation
+
+## Description
+A simple image encryption technique that modifies pixel values.
+
+## Features
+- Reads an image
+- Manipulates RGB values
+- Saves encrypted image
+
+## Requirements
+
+```bash
+pip install pillow
